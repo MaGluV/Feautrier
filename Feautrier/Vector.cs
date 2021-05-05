abd0a5ece0022863ce1a38003205fb9db9362b97
max <petrow-wodcin@yandex.ru>
2021-05-06 02:33:31 +0300

    Correction of Vector class

--- Feautrier/Vector.cs
+++ Feautrier/Vector.cs
@@ -18,20 +18,20 @@ namespace Feautrier
         	for(i=0; i<n; i++){vec[i] = 0;}
         }
         
-        public getValues()
+        public GetValues()
         {
         	return this.vec;
         }
         
-        public getSize()
+        public GetSize()
         {
         	return this.n;
         }
         
         public static Vector operator -(Vector v)
         {
-        	float[] new_vec = v.getValues();
-        	int m = v.getSize();
+        	float[] new_vec = v.GetValues();
+        	int m = v.GetSize();
         	int i;
         	for(i=0; i<m; i++){new_vec[i] = -new_vec[i];}
         	return new Vector(new_vec);
@@ -40,9 +40,9 @@ namespace Feautrier
         public static Vector operator +(Vector v1, Vector v2)
         {
         	float[] new_vec;
-        	float[] mas1 = v1.getValues();
-        	float[] mas2 = v2.getValues();
-        	int m = v1.getSize();
+        	float[] mas1 = v1.GetValues();
+        	float[] mas2 = v2.GetValues();
+        	int m = v1.GetSize();
         	int i;
         	for(i=0; i<m; i++){new_vec[i] = mas1[i] + mas2[i];}
         	return new Vector(new_vec);
@@ -53,9 +53,9 @@ namespace Feautrier
         public static float operator *(Vector v1, Vector v2)
         {
         	float mul=0;
-        	float[] mas1 = v1.getValues();
-        	float[] mas2 = v2.getValues();
-        	int m = v1.getSize();
+        	float[] mas1 = v1.GetValues();
+        	float[] mas2 = v2.GetValues();
+        	int m = v1.GetSize();
         	int i;
         	for(i=0; i<m; i++){mul += mas1[i]*mas2[i];}
         	return mul;
@@ -64,16 +64,17 @@ namespace Feautrier
         public static Vector operator *(Matrix mtx, Vector v)
         {
         	float[] new_vec;
-        	float[,] mas1 = mtx.getValues();
-        	float[] mas2 = v.getValues();
-        	int m = v.getSize();
+        	float[,] mas1 = mtx.GetValues();
+        	float[] mas2 = v.GetValues();
+        	int l = mtx.getHeight();
+        	int m = v.GetSize();
         	int i;
-        	for(i=0; i<m; i++)
+        	for(i=0; i<l; i++)
         	{
         		new_vec[i] = 0;
         		for(j=0; j<m; j++)
         		{
-        			new_vec[i] += mas1[i,j]*mas2[i];
+        			new_vec[i] += mas1[i,j]*mas2[j];
         		}
         	}
         	return new Vector(new_vec);
