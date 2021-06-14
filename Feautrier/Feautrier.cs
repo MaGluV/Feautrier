ed1f53f588d5b8aa81414ebb3bb5fa227c2574c5
max <petrow-wodcin@yandex.ru>
2021-06-14 19:08:26 +0300

    1/dt -> 1*dt

--- Feautrier/Feautrier.cs
+++ Feautrier/Feautrier.cs
@@ -49,17 +49,21 @@ namespace ColMod
             double[] dt = new double[this.m];
             double[] dtu = new double[this.m];
             double[] dtl = new double[this.m];
-            for (i = 0; i < this.m; i++) dt[i] = dtaus[i][0];
+            for (i = 0; i < this.m; i++) dt[i] = dtauU[i][0]; //dtaus[i][0];
             this.TauMu[0, 1] = this.NearBoundary(mu, dt);
-            for (i = 0; i < this.m; i++) dt[i] = dtauU[i][0];
-            this.TauMu[0, 2] = -this.LUMtxCreate(mu, dt, dt);
-            this.SourceFunc[0] = 0.5 * source[0] * this.SourceFunc[0];
+            for (i = 0; i < this.m; i++) dt[i] = 1.0; //dtauU[i][0];
+            for (i = 0; i < this.m; i++) dtu[i] = dtauU[i][0];
+            this.TauMu[0, 2] = -this.LUMtxCreate(mu, dt); //dt
+            this.SourceFunc[0] = 0.5 * SourceInit(source[0], dtu, dtu, dt);
+            //this.SourceFunc[0] = 0.5 * source[0] * this.SourceFunc[0];
 
-            for (i = 0; i < this.m; i++) dt[i] = dtaus[i][this.t-1];
+            for (i = 0; i < this.m; i++) dt[i] = dtauL[i][this.t-1]; //dtaus[i][this.t-1];
             this.TauMu[this.t - 1, 1] = this.FarBoundary(mu, dt);
-            for (i = 0; i < this.m; i++) dt[i] = dtauL[i][this.t - 2];
-            this.TauMu[this.t - 1, 0] = -this.LUMtxCreate(mu, dt, dt);
-            this.SourceFunc[this.t - 1] = 0.5 * source[this.t - 1] * this.SourceFunc[this.t - 1];
+            for (i = 0; i < this.m; i++) dt[i] = 1.0; //dtauL[i][this.t - 2];
+            for (i = 0; i < this.m; i++) dtl[i] = dtauL[i][this.t-1];
+            this.TauMu[this.t - 1, 0] = -this.LUMtxCreate(mu, dt); // dt
+            this.SourceFunc[this.t - 1] = 0.5 * SourceInit(source[this.t - 1], dtl, dtl, dt);
+            //this.SourceFunc[this.t - 1] = 0.5 * source[this.t - 1] * this.SourceFunc[this.t - 1];
 
             for (i = 1; i < this.t - 1; i++)
             {
@@ -69,15 +73,25 @@ namespace ColMod
                     dtu[j] = dtauU[j][i];   // <- особое внимание за этим выражением!
                     dtl[j] = dtauL[j][i-1]; // <- особое внимание за этим выражением!
                 }
-                this.TauMu[i, 0] = -this.LUMtxCreate(mu, dt, dtl);
+                this.TauMu[i, 0] = -this.LUMtxCreate(mu, dtu); // dt, dtl
                 //this.TauMu[i, 1] = new Matrix(this.m);
                 this.TauMu[i, 1] = this.DiagMtxCreate(mu, dt, dtl, dtu);
-                this.TauMu[i, 2] = -this.LUMtxCreate(mu, dt, dtu);
-                this.SourceFunc[i] = source[i] * this.SourceFunc[i];
+                this.TauMu[i, 2] = -this.LUMtxCreate(mu, dtl); //dt, dtu
+                this.SourceFunc[i] = SourceInit(source[i], dt, dtl, dtu);
+                //this.SourceFunc[i] = source[i] * this.SourceFunc[i];
             }
         }
+        
+        private Vector SourceInit(double s, double[] dtau, double[] dtau1, double[] dtau1)
+        {
+        	double[] S = new double[this.m];
+        	for (int i = 0; i < this.m; i++) S[i] = dtau[i]*dtau1[i]*dtau2[i]*s;
+        	
+        	return new Vector(S);
+        }
+        	
 
-        private Matrix LUMtxCreate(double[] mu, double[] dtau, double[] dtau1)
+        private Matrix LUMtxCreate(double[] mu, double[] dtau) //, double[] dtau1)
         {
             double[,] mtx = new double[this.m, this.m];
             int i;
@@ -87,7 +101,9 @@ namespace ColMod
             {
                 for (k = 0; k < this.m; k++)
                 {
-                    mtx[i, k] = (i == k) ? Math.Pow(mu[i], 2.0) / (dtau[i]*dtau1[i]) : 0; // Math.Pow(mu[i]/dtau, 2.0) : 0;
+                	mtx[i, k] = (i == k) ? Math.Pow(mu[i], 2.0)*dtau[i] : 0;
+                    //mtx[i, k] = (i == k) ? Math.Pow(mu[i], 2.0) / (dtau[i]*dtau1[i]) : 0; 
+                    // Math.Pow(mu[i]/dtau, 2.0) : 0;
                 }
             }
             return new Matrix(mtx);
@@ -103,7 +119,8 @@ namespace ColMod
             {
                 for (k = 0; k < this.m; k++)
                 {
-                    mtx[i, k] = (i == k) ? 1.0 + Math.Pow(mu[i], 2.0)*(1.0/dtau1[i] + 1.0/dtau2[i])/dtau[i] : 0; // Math.Pow(mu[i]/dtau, 2.0) : 0;
+                    mtx[i, k] = (i == k) ? dtau[i]*dtau1[i]*dtau2[i] + Math.Pow(mu[i], 2.0)*(dtau1[i] + dtau2[i]) : 0; 
+                    // Math.Pow(mu[i]/dtau, 2.0) : 0;
                 }
             }
             return new Matrix(mtx);
@@ -119,7 +136,7 @@ namespace ColMod
             {
                 for (k = 0; k < this.m; k++)
                 {
-                    mtx[i, k] = (i == k) ? 0.5 + mu[i] / dtau0[i] + Math.Pow(mu[i] / dtau0[i], 2.0) : 0; //0.5*Math.Pow(dtau0, 2.0) + mu[i] * dtau0 + Math.Pow(mu[i], 2.0) : 0; //
+                    mtx[i, k] = (i == k) ? 0.5*Math.Pow(dtau0[i], 2.0) + mu[i]*dtau0[i] + Math.Pow(mu[i], 2.0) : 0; //0.5*Math.Pow(dtau0, 2.0) + mu[i] * dtau0 + Math.Pow(mu[i], 2.0) : 0; //
                 }
             }
             return new Matrix(mtx);
@@ -135,7 +152,7 @@ namespace ColMod
             {
                 for (k = 0; k < this.m; k++)
                 {
-                    mtx[i, k] = (i == k) ? 0.5 + mu[i] / dtauN[i] + Math.Pow(mu[i] / dtauN[i], 2.0) : 0; //0.5 * Math.Pow(dtauN, 2.0) + mu[i] * dtauN + Math.Pow(mu[i], 2.0) : 0; //
+                    mtx[i, k] = (i == k) ? 0.5*Math.Pow(dtauN[i], 2.0) + mu[i]*dtauN[i] + Math.Pow(mu[i], 2.0) : 0; //0.5 * Math.Pow(dtauN, 2.0) + mu[i] * dtauN + Math.Pow(mu[i], 2.0) : 0; //
                 }
             }
             return new Matrix(mtx);
